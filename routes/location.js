815761e8c8b1773b88d9b810e9d465bdaee82be3
Sergey Arenzon <sergeyr1991@gmail.com>
2021-11-14 22:52:57 +0200

    add location post api

--- routes/location.js
+++ routes/location.js
@@ -21,5 +21,50 @@ router.get('/', async(req, res) => {
 });
 
 
+router.post('/', async(req, res) => {
+
+    const { name, price, location, description, email, images, coordinate } =
+      req.body;
+
+    // check for input validity
+    const isValid = await locationSchema.isValid({
+      name,
+      price,
+      location,
+      description,
+    });
+
+    if (!isValid) {
+      res.status(400).json({ message: "Error! Wrong input!" });
+    }
+
+    const author = await findOneUser(session.user.email);
+    const locationModel = new Location({
+      author: author._id,
+      email,
+      name,
+      price,
+      location,
+      description,
+      images,
+      coordinate
+    });
+
+    try {
+      const response = await locationModel.save();
+      res.status(201).json({
+        message: "Successfully location added.",
+        response,
+      });
+    } catch (error) {
+      res.status(501).json({
+        message: "Failed adding location",
+        error,
+      });
+    }
+    await locationModel.save();
+});
+
+
 export default router;
 
