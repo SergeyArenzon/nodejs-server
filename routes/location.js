6ea69c64b9ff2d19055f4612351838162db8e130
Sergey Arenzon <sergeyr1991@gmail.com>
2021-11-19 23:43:18 +0200

    complete adding routes

--- routes/location.js
+++ routes/location.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Location from "../models/Location.js";
-import { getAllLocations, findOneUser } from "../services/db.js";
+import { getAllLocations, findOneUser, getLocationById, deleteLocationById, updateLocationById, getCommentsByLocationId } from "../services/db.js";
 import { locationSchema } from "../validations/location.js";
 
 //  /location //
@@ -70,6 +70,7 @@ router.post('/', async(req, res) => {
 
 router.get("/:id", async(req, res) => {
    const { id } = req.params;
+   console.log(id);
     try {
       const location = await getLocationById(id);
       res.status(200).json({
@@ -80,6 +81,148 @@ router.get("/:id", async(req, res) => {
       res.status(400).json({ message: error });
     }
 })
+
+
+router.delete("/:id", async(res, req)=> {
+    const { id } = req.params;
+    const session = await getSession({ req });
+    console.log(session.user.email);
+    const location = await getLocationById(id);
+    console.log(session.user.email, location.email);
+
+    if (session.user.email !== location.email) {
+      res.status(401).json({ message: "Wrong user trying to delete location" });
+      return;
+    }
+    try {
+      const respone = await deleteLocationById(id);
+      res.status(200).json({
+        message: "Successfully location Deleted by id",
+        location: respone,
+      });
+    } catch (error) {
+      res.status(400).json({ message: error });
+    }
+
+});
+
+
+router.put('/:id/edit', async(res, req) => {
+  
+  const { id } = req.params;
+  const updatedData = req.body;
+
+  // Check for correct user trying to edit 
+  const location = await getLocationById(id);
+  if(location.email !== session.user.email){
+      res.status(401).json({message: "Wrong user editing location"});
+      return;
+  }
+  try {
+      const response = await updateLocationById(id, updatedData);
+
+      res.status(200).json({
+          message: "Successfully location by id updated",
+          location: response,
+      });
+  } catch (error) {
+      res.status(400).json({ message: error });
+  }
+})
+
+
+
+
+router.put('/:id/rating', async(req, res) => {
+  const userEmail = session.user.email;
+  const rating = req.body.rating;
+  const locationId = req.params.id;
+  const location = await getLocationById(locationId);
+
+  const user = await findOneUser(userEmail);
+
+  let userAlreadtRated = false;
+  try {
+    location.ratings.forEach((existRating, index) => {
+      if (existRating.user._id.equals(user._id)) {
+        location.ratings[index].rating = rating;
+        userAlreadtRated = true;
+      }
+    });
+
+    if (!userAlreadtRated) {
+      location.ratings.push({ user, rating });
+    }
+    await updateLocationById(locationId, location);
+    res.status(200).json({
+      message: "Successfully rating was added",
+      rating,
+      location,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed adding rating",
+      rating,
+      location,
+    });
+  }
+})
+
+router.post('/:id/comment', async(req, res) => {
+  const { id } = req.params;
+  const { title, body } = req.body;
+  const locationId = id;
+  const creatorEmail = session.user.email;
+  const author = await findOneUser(creatorEmail);
+  const location = await getLocationById(locationId);
+
+  // console.log(location);
+  const comment = new Comment({
+      author: author._id,
+      title,
+      body,
+  });
+
+  try {
+      const response = await comment.save();
+
+      location.comments.push(comment);
+      location.save();
+
+      //  const l = await Location.findOne({name: "test2"}).populate('comments').exec()
+      //    console.log(l)
+
+      res.status(201).json({
+          message: "Successfully comment added.",
+          response,
+      });
+  } catch (error) {
+      console.log(error);
+      res.status(501).json({
+          message: "Failed adding comment",
+          error,
+      });
+  }
+});
+
+
+router.get('/:id/comment', async(req, res) => {
+  const { id } = req.params;
+
+  try{
+      const comments = await getCommentsByLocationId(id);
+      res.status(200).json({
+          message: "Successfully all comments fetched.",
+          comments: comments,
+      });
+  
+  }catch(error){
+      res.status(501).json({
+          message: "Failed adding comment",
+          error,
+      });
+  }
+});
 
 
 export default router;
